d186f2c4432b6c18ffaa1c842a7d193fe3413966
psycheguy <yiwenmai2005@gmail.com>
2025-12-17 21:41:19 +0100

    files organized

--- app/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# start.sh
-uvicorn main:app --host 0.0.0.0 --port 8000
--- postgres/init/query.sql
+++
@@ -1,7 +0,0 @@
-\dt;
-SELECT * FROM customers;
-SELECT * FROM categories;
-SELECT * FROM products;
-SELECT * FROM orders;
-SELECT * FROM order_items;
-SELECT * FROM events;
